70bd2de9c59ca59d0898c43592eb395d13000f24
Apdy <yanralapdy.xxx@gmail.com>
2021-06-30 12:08:58 +0700

    update migrate.py, delete db before migrate

--- migrate.py
+++ migrate.py
@@ -1,5 +1,10 @@
 from app.Databases.migrate import migrate
 from app.Config.Connect import Connect
+import os
+filePath = './app/Databases/kelompok_tiga.db'
+# As file at filePath is deleted now, so we should check if file exists or not not before deleting them
+if os.path.exists(filePath):
+    os.remove(filePath)
 
 conn = Connect.instance_method()
 
